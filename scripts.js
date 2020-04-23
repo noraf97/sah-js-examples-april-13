f7687af601548c3aae90a9801ece3643a4ea581c
norafekeshazy <63554669+norafekeshazy@users.noreply.github.com>
2020-04-23 17:03:29 +0200

    javascript

---
+++ scripts.js
@@ -0,0 +1,99 @@
+let name = 'Adam';
+
+console.log(name + "'s apples")
+console.log ('Hello World!')
+
+// this a JavaScript comment
+
+/*let age
+undefined
+age = 33
+33
+let name = Adam
+VM112:1 Uncaught ReferenceError: Adam is not defined
+    at <anonymous>:1:12
+(anonymous) @ VM112:1
+let name = 'Adam'
+undefined
+name
+"Adam"
+name = "Peti"
+"Peti"
+"Adam's apples"
+"Adam's apples"
+true
+true
+false
+false
+let oldEnoughToDrive = false
+undefined
+oldEnoughToDrive = true
+true
+age +5
+38
+name + 'ish'
+"Petiish"
+true && true
+true
+false && true
+false
+true || true
+true
+true || false
+true
+true && (false || (true && false))
+false
+!true
+false
+!false
+true
+oldEnoughToDrive
+true
+!oldEnoughToDrive
+false
+let color1 = 'orange
+VM830:1 Uncaught SyntaxError: Invalid or unexpected token
+let colors = ['orange', 'blue', 'green']
+undefined
+colors
+(3) ["orange", "blue", "green"]
+colors [1]
+"blue"
+colors.length
+3
+colors.push ('yellow')
+4
+colors
+(4) ["orange", "blue", "green", "yellow"]
+[colors, colors, colors]
+(3) [Array(4), Array(4), Array(4)]
+colors [3] = 'lime'
+"lime"
+colors
+(4) ["orange", "blue", "green", "lime"]
+let person ={
+  name: 'Adam',
+  age: 33,
+}
+undefined
+let person ={
+  name: 'Adam',
+  age: 33,
+  oldEnoughToDrive: true,
+}
+undefined
+person
+{name: "Adam", age: 33, oldEnoughToDrive: true}
+persone.name
+VM1351:1 Uncaught ReferenceError: persone is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM1351:1
+person.name
+"Adam"
+person.age
+33
+person.favoriteColors = ['lime', 'green']
+(2) ["lime", "green"]
+person
+{name: "Adam", age: 33, oldEnoughToDrive: true, favoriteColors: Array(2)}
+*/
